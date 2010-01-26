e2c97998f19ed18495950a04477bce90bdbfdb5a
Bradley Beddoes <beddoes@intient.com>
2010-01-27 09:48:11 +1000

    Added hibernate configuration to ensure DB connection stays alive

--- federationregistry/grails-app/realms/fedreg/host/ShibbolethRealm.groovy
+++ federationregistry/grails-app/realms/fedreg/host/ShibbolethRealm.groovy
@@ -24,56 +24,56 @@ class ShibbolethRealm {
 	def grailsApplication
 	
 	def authenticate(authToken) {
-        if (!grailsApplication.config.fedreg.shibboleth.federationprovider.enabled) {
-            log.error("Authentication attempt for Shibboleth provider, denying attempt as Shibboleth disabled")
-            throw new UnknownAccountException("Authentication attempt for Shibboleth provider, denying attempt as Shibboleth disabled")
-        }
+		if (!grailsApplication.config.fedreg.shibboleth.federationprovider.enabled) {
+			log.error("Authentication attempt for Shibboleth provider, denying attempt as Shibboleth disabled")
+			throw new UnknownAccountException("Authentication attempt for Shibboleth provider, denying attempt as Shibboleth disabled")
+		}
 
 		if (!authToken.principal) {
-            log.error("Authentication attempt for Shibboleth provider, denying attempt as no persistent identifier (ShibbolethToken.principal) was provided")
-            throw new UnknownAccountException("Authentication attempt for Shibboleth provider, denying attempt as no persistent identifier (ShibbolethToken.principal) was provided")
-        }
+			log.error("Authentication attempt for Shibboleth provider, denying attempt as no persistent identifier (ShibbolethToken.principal) was provided")
+			throw new UnknownAccountException("Authentication attempt for Shibboleth provider, denying attempt as no persistent identifier (ShibbolethToken.principal) was provided")
+		}
 
 		if (!authToken.entityID) {
-            log.error("Authentication attempt for Shibboleth provider, denying attempt as no entityID (ShibbolethToken.entityID) was provided")
-            throw new UnknownAccountException("Authentication attempt for Shibboleth provider, denying attempt as no entityID (ShibbolethToken.entityID) was provided")
-        }
+			log.error("Authentication attempt for Shibboleth provider, denying attempt as no entityID (ShibbolethToken.entityID) was provided")
+			throw new UnknownAccountException("Authentication attempt for Shibboleth provider, denying attempt as no entityID (ShibbolethToken.entityID) was provided")
+		}
 
 		def user = UserBase.findByUsername(authToken.principal)
-        if (!user) {
-            log.info("No account representing user ${authToken.principal} exists")
+		if (!user) {
+			log.info("No account representing user ${authToken.principal} exists")
 			def shibbolethFederationProvider = FederationProvider.findByUid(ShibbolethService.federationProviderUid)
 			if (shibbolethFederationProvider && shibbolethFederationProvider.autoProvision) {	
-                log.info("Shibboleth auto provision is enabled, creating user account for ${authToken.principal} belonging to Entity ${authToken.entityID}")
+				log.info("Shibboleth auto provision is enabled, creating user account for ${authToken.principal} belonging to Entity ${authToken.entityID}")
 
 				def entityDescriptor = EntityDescriptor.findWhere(entityID:authToken.entityID)
 				if(!entityDescriptor) {
 					log.error("Authentication attempt for Shibboleth provider, denying attempt as no Entity matching (ShibbolethToken.entityID) is available. Has bootstrap occured?")
-		            throw new UnknownAccountException("Authentication attempt for Shibboleth provider, denying attempt as no Entity matching (ShibbolethToken.entityID) is available. Has bootstrap occured?")
+					throw new UnknownAccountException("Authentication attempt for Shibboleth provider, denying attempt as no Entity matching (ShibbolethToken.entityID) is available. Has bootstrap occured?")
 				}
 
 				UserBase newUser = InstanceGenerator.user()
-	            newUser.username = authToken.principal
-	            newUser.enabled = true
-	            newUser.external = true
-	            newUser.federated = true
+				newUser.username = authToken.principal
+				newUser.enabled = true
+				newUser.external = true
+				newUser.federated = true
 				newUser.federationProvider = shibbolethFederationProvider
 				newUser.entityDescriptor = entityDescriptor
 			
 				newUser.profile = InstanceGenerator.profile()
-                newUser.profile.owner = newUser
-                newUser.profile.fullName = "${authToken.givenName} ${authToken.surname}"
+				newUser.profile.owner = newUser
+				newUser.profile.fullName = "${authToken.givenName} ${authToken.surname}"
 				newUser.profile.email = (authToken.email == "") ? null : authToken.email
 					
 				user = userService.createUser(newUser)
-                if (user.hasErrors()) {
-                    log.error("Error creating user account from Shibboleth credentials for ${authToken.principal}")
-                    user.errors.each {
-                        log.error(it)
-                    }
-                    throw new RuntimeException("Account creation exception for new shibboleth based account");
-                }
-                log.info("Created new user [$user.id]$user.username from Shibboleth attribute statement")
+				if (user.hasErrors()) {
+					log.error("Error creating user account from Shibboleth credentials for ${authToken.principal}")
+					user.errors.each {
+						log.error(it)
+					}
+					throw new RuntimeException("Account creation exception for new shibboleth based account");
+				}
+				log.info("Created new user [$user.id]$user.username from Shibboleth attribute statement")
 
 				// To assist with bootstrap provide the first account with admin privilledges
 				// ==1 because we created and saved above
@@ -83,12 +83,12 @@ class ShibbolethRealm {
 				}
 			}
 			else
-            	throw new UnknownAccountException("No account representing user $username exists and autoProvision is false")
+				throw new UnknownAccountException("No account representing user $username exists and autoProvision is false")
 		}else {
 			// Update name and email to what IDP has supplied - this could be extended to role membership etc in the future
 			boolean change = false
 			def fullName = "${authToken.givenName} ${authToken.surname}"
-			def email = authToken.email	
+			def email = authToken.email 
 			if(user.profile.fullName != fullName) {
 				change = true
 				user.profile.fullName = fullName
@@ -104,13 +104,13 @@ class ShibbolethRealm {
 		}
 		
 		if (!user.enabled) {
-            log.warn("Attempt to authenticate using using Shibboleth with locally disabled account [$user.id]$user.username")
-            throw new DisabledAccountException("The account [$user.id]$user.username accessed via Shibboleth is disabled")
-        }
+			log.warn("Attempt to authenticate using using Shibboleth with locally disabled account [$user.id]$user.username")
+			throw new DisabledAccountException("The account [$user.id]$user.username accessed via Shibboleth is disabled")
+		}
 		log.info("Successfully logged in user [$user.id]$user.username using Shibboleth")
-        def account = new SimpleAccount(user.id, authToken.principal, "redreg.realm.ShibbolethRealm")
-        return account
-    }
+		def account = new SimpleAccount(user.id, authToken.principal, "redreg.realm.ShibbolethRealm")
+		return account
+	}
 
 	public String toString() {
 		"Realm: fedreg.host.ShibbolethRealm"
