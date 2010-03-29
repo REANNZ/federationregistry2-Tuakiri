e2333f10150ce7022dcd2578d43bbaf690ae29ff
Bradley Beddoes <beddoes@intient.com>
2010-03-29 12:29:18 +1000

    Removed Nimble as a submodule in preperation to support native Grails plugin resolution as codebase has stabilized

--- federationregistry/grails-app/conf/BuildConfig.groovy
+++ federationregistry/grails-app/conf/BuildConfig.groovy
@@ -2,7 +2,6 @@
 // Local Plugins
 grails.plugin.location.federationcore="../federationcore"
 grails.plugin.location.federationcompliance="../federationcompliance"
-grails.plugin.location.nimble="../nimble"
 
 grails.project.class.dir = "target/classes"
 grails.project.test.class.dir = "target/test-classes"
