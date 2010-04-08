b939165bcb1342a339fc2dabb89e9e71e4e7e8ce
Bradley Beddoes <beddoes@intient.com>
2010-04-08 11:52:33 +1000

    Fixed bug in Attribute Release comparison

--- federationcompliance/grails-app/controllers/fedreg/compliance/AttributeReleaseController.groovy
+++ federationcompliance/grails-app/controllers/fedreg/compliance/AttributeReleaseController.groovy
@@ -5,24 +5,8 @@ import fedreg.core.SPSSODescriptor
 
 class AttributeReleaseController {
 	
-	def index = {
-		
-		def c = IDPSSODescriptor.createCriteria()
-		def aIDP = c.list {
-			entityDescriptor {
-				eq("active", true)
-			}
-		}
-		
-		c = SPSSODescriptor.createCriteria()
-		def aSP = c.list {
-			entityDescriptor {
-				eq("active", true)
-			}
-		}
-		
-		[activeIDP:aIDP, activeSP:aSP]
-		
+	def index = {	
+		[activeIDP:IDPSSODescriptor.findAllWhere(active:true), activeSP:SPSSODescriptor.findAllWhere(active:true)]
 	}
 	
 	def compare = {
