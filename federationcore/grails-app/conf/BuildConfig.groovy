2b9bc8323e4bdb7eeefeb391defe4f393f203dc9
Bradley Beddoes <beddoes@intient.com>
2010-07-07 12:46:30 +1000

    introduced workflow as a dependency of core

--- federationcore/grails-app/conf/BuildConfig.groovy
+++ federationcore/grails-app/conf/BuildConfig.groovy
@@ -1,4 +1,7 @@
 
+// Local Plugins
+grails.plugin.location.federationworkflow="../federationworkflow"
+
 grails.project.class.dir = "target/classes"
 grails.project.test.class.dir = "target/test-classes"
 grails.project.test.reports.dir	= "target/test-reports"
