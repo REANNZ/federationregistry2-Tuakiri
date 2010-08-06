caa589e6d2e7c0bfe0517000ef549daeda93b523
Bradley Beddoes <beddoes@intient.com>
2010-08-06 15:44:39 +1000

    Update to service description to better support requirements of service registry

--- federationcore/grails-app/domain/fedreg/core/ServiceDescription.groovy
+++ federationcore/grails-app/domain/fedreg/core/ServiceDescription.groovy
@@ -23,10 +23,19 @@ package fedreg.core
  * @author Bradley Beddoes
  */
 class ServiceDescription {
-	
-	//TODO: Flesh out more to produce publicly facing content for service catalogue
-	
 	boolean publish = false
+	
+	String connectURL		// URL for end users to access service
+	String logo				// URL to image of the services logo
+	
+	String furtherInfo		// Further information URL
+	String provides			// What the Service Provides
+	String benefits			// Benefits of using the service
+	String audience			// Intended audience
+	String restrictions		// Usage restrictions
+	String accessing		// Accessing the service
+	String support			// Available support
+	String maintenance		// Maintenance periods, generally expected unavailability
 	
 	Date dateCreated
 	Date lastUpdated
@@ -34,10 +43,19 @@ class ServiceDescription {
 	static belongsTo = [spSSODescriptor:SPSSODescriptor]
 	
 	static constraints = {
+		connectURL(nullable:true)
+		logo(nullable:true)
+		furtherInfo(nullable:true)
+		provides(nullable:true)
+		benefits(nullable:true)
+		audience(nullable:true)
+		restrictions(nullable:true)
+		accessing(nullable:true)
+		support(nullable:true)
+		maintenance(nullable:true)
 		dateCreated(nullable:true)
 		lastUpdated(nullable:true)
 	}
 	
-	public String toString() {	"servicedescription:[id:$id]" }
-	
+	public String toString() {	"servicedescription:[id:$id, publish:$publish]" }
 }
