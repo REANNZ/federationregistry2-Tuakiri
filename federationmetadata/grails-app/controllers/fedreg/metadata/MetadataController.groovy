8da2566c5a925098d8e54bc0ddbc6f4e209890fa
Bradley Beddoes <beddoes@intient.com>
2010-08-27 09:47:31 +1000

    Created initial controller to render complete metadata document

--- federationmetadata/grails-app/controllers/fedreg/metadata/MetadataController.groovy
+++ federationmetadata/grails-app/controllers/fedreg/metadata/MetadataController.groovy
@@ -1,14 +1,27 @@
 package fedreg.metadata
 
-
-
+import groovy.xml.MarkupBuilder
 import fedreg.core.*
 
 class MetadataController {
 	def metadataGenerationService
+	def grailsApplication
 	
 	def extensive = {
-
+		def now = new Date();
+		def validUntil = now + grailsApplication.config.fedreg.metadata.extensive.validForDays
+		def cacheDuration = now + grailsApplication.config.fedreg.metadata.extensive.cacheForDays
+		def federation = grailsApplication.config.fedreg.metadata.federation
+		def certificateAuthorities = CAKeyInfo.list()
+		
+		def writer = new StringWriter()
+		def builder = new MarkupBuilder(writer)
+		
+		def entitiesDescriptor = new EntitiesDescriptor(name:federation)
+		
+		metadataGenerationService.entitiesDescriptor(builder, entitiesDescriptor, validUntil, cacheDuration, certificateAuthorities)
+		def xml = writer.toString()
+		render(text:xml, contentType:"text/xml", encoding:"UTF-8")
 	}
 		
 }
