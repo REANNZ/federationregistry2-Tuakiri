413ec8ea58020dc6a62e8e2fbfcc5bcdab20f04f
Bradley Beddoes <beddoes@intient.com>
2010-09-08 15:22:01 +1000

    Integrate permissions to DescriptorNameIDFormatController

--- federationcore/grails-app/controllers/fedreg/core/DescriptorNameIDFormatController.groovy
+++ federationcore/grails-app/controllers/fedreg/core/DescriptorNameIDFormatController.groovy
@@ -1,5 +1,7 @@
 package fedreg.core
 
+import org.apache.shiro.SecurityUtils
+
 class DescriptorNameIDFormatController {
 
 	static allowedMethods = [remove: "POST"]
@@ -26,35 +28,40 @@ class DescriptorNameIDFormatController {
 			response.setStatus(500)
 			return
 		}
-		
-		def nameIDFormat = SamlURI.get(params.formatID)
-		if(!nameIDFormat) {
-			log.warn "NameIDFormat identified by id $params.formatID was not located"
-			render message(code: 'fedreg.nameidformat.nonexistant', args: [params.formatID])
-			response.setStatus(500)
-			return
-		}
-		
-		if(!descriptor.nameIDFormats.contains(nameIDFormat)) {
-			log.warn "NameIDFormat identified by id $params.formatID was already supported by descriptor ${params.id}"
-			response.setStatus(500)
-			render message(code: 'fedreg.nameidformat.remove.notsupported', args:[nameIDFormat.uri])
-			return
-		}
-		
-		log.info "Removing nameIDFormat (${params.formatID})${nameIDFormat.uri} from descriptor ${params.id}"
-		descriptor.removeFromNameIDFormats(nameIDFormat)
-		descriptor.save()
-		if(descriptor.hasErrors()) {
-			log.warn "Removing nameIDFormat (${params.formatID})${nameIDFormat.uri} from descriptor ${params.id} failed"
-			descriptor.errors.each {
-				log.debug it
+		if(SecurityUtils.subject.isPermitted("descriptor:${descriptor.id}:nameidformat:remove")) {
+			def nameIDFormat = SamlURI.get(params.formatID)
+			if(!nameIDFormat) {
+				log.warn "NameIDFormat identified by id $params.formatID was not located"
+				render message(code: 'fedreg.nameidformat.nonexistant', args: [params.formatID])
+				response.setStatus(500)
+				return
 			}
-			render message(code: 'fedreg.nameidformat.remove.failed', args:[nameIDFormat.uri])
-			response.setStatus(500)
-			return
-		}else {
-			render message(code: 'fedreg.nameidformat.remove.success', args:[nameIDFormat.uri])
+		
+			if(!descriptor.nameIDFormats.contains(nameIDFormat)) {
+				log.warn "NameIDFormat identified by id $params.formatID was already supported by descriptor ${params.id}"
+				response.setStatus(500)
+				render message(code: 'fedreg.nameidformat.remove.notsupported', args:[nameIDFormat.uri])
+				return
+			}
+		
+			log.info "Removing nameIDFormat (${params.formatID})${nameIDFormat.uri} from descriptor ${params.id}"
+			descriptor.removeFromNameIDFormats(nameIDFormat)
+			descriptor.save()
+			if(descriptor.hasErrors()) {
+				log.warn "Removing nameIDFormat (${params.formatID})${nameIDFormat.uri} from descriptor ${params.id} failed"
+				descriptor.errors.each {
+					log.debug it
+				}
+				render message(code: 'fedreg.nameidformat.remove.failed', args:[nameIDFormat.uri])
+				response.setStatus(500)
+				return
+			}else {
+				render message(code: 'fedreg.nameidformat.remove.success', args:[nameIDFormat.uri])
+			}
+		}
+		else {
+			log.warn("Attempt to remove NameIDFormat from $descriptor by $authenticatedUser was denied, incorrect permission set")
+			response.sendError(403)
 		}
 	}
 	
@@ -107,33 +114,39 @@ class DescriptorNameIDFormatController {
 			return
 		}
 		
-		def nameIDFormat = SamlURI.get(params.formatID)
-		if(!nameIDFormat) {
-			log.warn "NameIDFormat identified by id $params.formatID was not located"
-			render message(code: 'fedreg.nameidformat.nonexistant', args: [params.formatID])
-			response.setStatus(500)
-			return
-		}
-		
-		if(descriptor.nameIDFormats.contains(nameIDFormat)) {
-			log.warn "NameIDFormat identified by id $params.formatID was already supported by descriptor ${params.id}"
-			response.setStatus(500)
-			render message(code: 'fedreg.nameidformat.add.alreadysupported', args:[nameIDFormat.uri])
-			return
-		}
-		
-		descriptor.addToNameIDFormats(nameIDFormat)
-		descriptor.save()
-		if(descriptor.hasErrors()) {
-			log.warn "Adding nameIDFormat (${params.formatID})${nameIDFormat.uri} to descriptor ${params.id} failed"
-			descriptor.errors.each {
-				log.debug it
+		if(SecurityUtils.subject.isPermitted("descriptor:${descriptor.id}:nameidformat:add")) {
+			def nameIDFormat = SamlURI.get(params.formatID)
+			if(!nameIDFormat) {
+				log.warn "NameIDFormat identified by id $params.formatID was not located"
+				render message(code: 'fedreg.nameidformat.nonexistant', args: [params.formatID])
+				response.setStatus(500)
+				return
 			}
-			render message(code: 'fedreg.nameidformat.add.failed', args:[nameIDFormat.uri])
-			response.setStatus(500)
-			return
-		}else {
-			render message(code: 'fedreg.nameidformat.add.success', args:[nameIDFormat.uri])
+		
+			if(descriptor.nameIDFormats.contains(nameIDFormat)) {
+				log.warn "NameIDFormat identified by id $params.formatID was already supported by descriptor ${params.id}"
+				response.setStatus(500)
+				render message(code: 'fedreg.nameidformat.add.alreadysupported', args:[nameIDFormat.uri])
+				return
+			}
+		
+			descriptor.addToNameIDFormats(nameIDFormat)
+			descriptor.save()
+			if(descriptor.hasErrors()) {
+				log.warn "Adding nameIDFormat (${params.formatID})${nameIDFormat.uri} to descriptor ${params.id} failed"
+				descriptor.errors.each {
+					log.debug it
+				}
+				render message(code: 'fedreg.nameidformat.add.failed', args:[nameIDFormat.uri])
+				response.setStatus(500)
+				return
+			}else {
+				render message(code: 'fedreg.nameidformat.add.success', args:[nameIDFormat.uri])
+			}
+		}
+		else {
+			log.warn("Attempt to add NameIDFormat to $descriptor by $authenticatedUser was denied, incorrect permission set")
+			response.sendError(403)
 		}
 	}
 
