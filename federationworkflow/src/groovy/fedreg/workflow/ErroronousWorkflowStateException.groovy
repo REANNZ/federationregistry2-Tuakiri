15edf08145bd1835f523c3544a09d14fb9e17ee6
Bradley Beddoes <beddoes@intient.com>
2010-12-02 15:38:14 +1000

    Updated exception classes to inlude correct constructions ... duh.

--- federationworkflow/src/groovy/fedreg/workflow/ErroronousWorkflowStateException.groovy
+++ federationworkflow/src/groovy/fedreg/workflow/ErroronousWorkflowStateException.groovy
@@ -2,4 +2,12 @@ package fedreg.workflow
 
 class ErronousWorkflowStateException extends RuntimeException {
 	
+	ErronousWorkflowStateException() { super() }
+
+	ErronousWorkflowStateException(String message) { super(message) } 
+
+	ErronousWorkflowStateException(String message, Throwable cause) { super(message, cause) }
+
+	ErronousWorkflowStateException(Throwable cause) { super(cause) }
+	
 }
