80f53de8ca46b2bd4bae00f98c6e6d7b759e0408
Bradley Beddoes <beddoes@intient.com>
2010-12-02 15:35:16 +1000

    Updated exception classes to inlude correct constructions ... duh.

--- federationcore/src/groovy/fedreg/core/ErronousStateException.groovy
+++ federationcore/src/groovy/fedreg/core/ErronousStateException.groovy
@@ -2,4 +2,12 @@ package fedreg.core
 
 class ErronousStateException extends RuntimeException {
 	
+	ErronousStateException() { super() }
+
+	ErronousStateException(String message) { super(message) } 
+
+	ErronousStateException(String message, Throwable cause) { super(message, cause) }
+
+	ErronousStateException(Throwable cause) { super(cause) }
+	
 }
--- federationmetadata/src/groovy/fedreg/metadata/ErroronousStateException.groovy
+++ federationmetadata/src/groovy/fedreg/metadata/ErroronousStateException.groovy
@@ -2,4 +2,12 @@ package fedreg.metadata
 
 class ErronousStateException extends RuntimeException {
 	
+	ErronousStateException() { super() }
+
+	ErronousStateException(String message) { super(message) } 
+
+	ErronousStateException(String message, Throwable cause) { super(message, cause) }
+
+	ErronousStateException(Throwable cause) { super(cause) }
+	
 }
