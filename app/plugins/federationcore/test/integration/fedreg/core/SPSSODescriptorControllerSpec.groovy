0ae09c49e307e433443be9dab6c5b6b3d988d9dd
Bradley Beddoes <bradleybeddoes@gmail.com>
2011-11-16 10:00:20 +1000

    streamlined and corrected multiple cases

--- app/plugins/federationcore/test/integration/fedreg/core/SPSSODescriptorControllerSpec.groovy
+++ app/plugins/federationcore/test/integration/fedreg/core/SPSSODescriptorControllerSpec.groovy
@@ -8,29 +8,18 @@ import grails.plugins.nimble.core.*
 
 class SPSSODescriptorControllerSpec extends IntegrationSpec {
 	
-	def controller
-	def savedMetaClasses
-	def spssoDescriptorService = new SPSSODescriptorService()
+    def user, spssoDescriptorService, controller
 	
 	def cleanup() {
-		SpecHelpers.resetMetaClasses(savedMetaClasses)
-		spssoDescriptorService.metaClass = SPSSODescriptorService.metaClass
+       user.perms = []
 	}
 	
 	def setup () {
-		savedMetaClasses = [:]
-		
-		SpecHelpers.registerMetaClass(SPSSODescriptorService, savedMetaClasses)
-		spssoDescriptorService.metaClass = SPSSODescriptorService.metaClass
-		
-		controller = new SPSSODescriptorController(SPSSODescriptorService:spssoDescriptorService)
-		def user = UserBase.build()
+        spssoDescriptorService = new SPSSODescriptorService()
+        controller = new SPSSODescriptorController(SPSSODescriptorService:spssoDescriptorService)
+
+		user = UserBase.build()
 		SpecHelpers.setupShiroEnv(user)
-		
-		// Clear storage - odd issue with 1.3.6 have not yet confirmed where bug lies
-		SPSSODescriptor.findAll()*.delete(flush:true)
-		EntityDescriptor.findAll()*.delete(flush:true)
-		Organization.findAll()*.delete(flush:true)
 	}
 	
 	def setupBindings() {
@@ -53,44 +42,7 @@ class SPSSODescriptorControllerSpec extends IntegrationSpec {
 		def model = controller.list()
 
 		then:
-		model.serviceProviderList.size() == 20
-	}
-	
-	def "Validate list with max set"() {
-		setup:
-		(1..25).each { i ->
-			def ed = EntityDescriptor.build(entityID:"http://sp.test.com/$i")
-			def sp = SPSSODescriptor.build(entityDescriptor: ed)
-			sp.save()
-		}
-		controller.params.max = 10
-		
-		when:
-		def model = controller.list()
-
-		then:
-		model.serviceProviderList.size() == 10
-		model.serviceProviderList.get(0) == SPSSODescriptor.list().get(0)
-		model.serviceProviderList.get(9) == SPSSODescriptor.list().get(9)
-	}
-	
-	def "Validate list with max and offset set"() {
-		setup:
-		(1..25).each { i->
-			def ed = EntityDescriptor.build(entityID:"http://sp.test.com/$i")
-			def sp = SPSSODescriptor.build(entityDescriptor: ed)
-			sp.save()
-		}
-		controller.params.max = 10
-		controller.params.offset = 5
-		
-		when:
-		def model = controller.list()
-
-		then:
-		model.serviceProviderList.size() == 10
-		model.serviceProviderList.get(0) == SPSSODescriptor.list().get(5)
-		model.serviceProviderList.get(9) == SPSSODescriptor.list().get(14)
+		model.serviceProviderList.size() == 25
 	}
 	
 	def "Show with no ID"() {		
@@ -105,21 +57,21 @@ class SPSSODescriptorControllerSpec extends IntegrationSpec {
 	
 	def "Show with invalid SPSSODescriptor ID"() {
 		setup:
-		controller.params.id = 2
+		controller.params.id = 200000000
 			
 		when:
 		controller.show()
 		
 		then:
-		controller.flash.type == "error"
+        controller.flash.type == "error"
 		controller.flash.message == "fedreg.core.spssoroledescriptor.nonexistant"
-		controller.response.redirectedUrl == "/SPSSODescriptor/list"
+        controller.response.redirectedUrl == "/SPSSODescriptor/list"
 	}
 	
 	def "Validate create"() {
 		setup:
 		(1..10).each {
-			Organization.build().save()
+			Organization.build(active:true, approved:true).save()
 		}
 		
 		(1..11).each { i ->
@@ -147,11 +99,12 @@ class SPSSODescriptorControllerSpec extends IntegrationSpec {
 		def entityDescriptor = EntityDescriptor.build(organization:organization).save()
 		def serviceProvider = SPSSODescriptor.build(entityDescriptor:entityDescriptor).save()
 		
+        spssoDescriptorService.metaClass.create = { def p -> 
+            return [true, [organization:organization, entityDescriptor:entityDescriptor, serviceProvider:serviceProvider]]  
+            //.. deliberately leaving out other return vals here
+        } 
+
 		when:
-		spssoDescriptorService.metaClass.create = { def p -> 
-			println "X"
-			return [true, organization, entityDescriptor, serviceProvider]	//.. deliberately leaving out other return vals here
-		} 
 		def model = controller.save()
 		
 		then:
@@ -160,20 +113,22 @@ class SPSSODescriptorControllerSpec extends IntegrationSpec {
 	
 	def "Validate failed save"() {
 		setup:
-		def organization = Organization.build().save()
-		def entityDescriptor = EntityDescriptor.build(organization:organization).save()
-		def serviceProvider = SPSSODescriptor.build(entityDescriptor:entityDescriptor).save()
+		def organization = Organization.build()
+		def entityDescriptor = EntityDescriptor.build(organization:organization)
+		def serviceProvider = SPSSODescriptor.build(entityDescriptor:entityDescriptor)
 		
+        spssoDescriptorService.metaClass.create = { def p -> 
+            return [false, [organization:organization, entityDescriptor:entityDescriptor, serviceProvider:serviceProvider]] 
+            //.. deliberately leaving out other return vals here
+        } 
+
 		when:
-		spssoDescriptorService.metaClass.create = { def p -> 
-			return [false, organization, entityDescriptor, serviceProvider]	//.. deliberately leaving out other return vals here
-		} 
 		def model = controller.save()
 		
 		then:
-		//organization == controller.modelAndView.model.organization	
-		//entityDescriptor == controller.modelAndView.model.entityDescriptor	
-		//serviceProvider == controller.modelAndView.model.serviceProvider
+		organization == controller.modelAndView.model.organization	
+		entityDescriptor == controller.modelAndView.model.entityDescriptor	
+		serviceProvider == controller.modelAndView.model.serviceProvider
 		
 		controller.flash.type == "error"
 		controller.flash.message == "fedreg.core.spssoroledescriptor.save.validation.error"	
@@ -181,21 +136,39 @@ class SPSSODescriptorControllerSpec extends IntegrationSpec {
 	
 	def "Validate successful update"() {
 		setup:
-		def organization = Organization.build().save()
-		def entityDescriptor = EntityDescriptor.build(organization:organization).save()
+		def organization = Organization.build()
+		def entityDescriptor = EntityDescriptor.build(organization:organization)
 		def serviceProvider = SPSSODescriptor.build(entityDescriptor:entityDescriptor).save()
 		
 		controller.params.id = serviceProvider.id
-		
-		when:
+        user.perms.add("descriptor:${serviceProvider.id}:update")
 		spssoDescriptorService.metaClass.update = { def p -> 
-			return [true, serviceProvider]
-		} 
+            return [true, serviceProvider]
+        } 
+
+		when:
 		def model = controller.update()
 		
 		then:
 		controller.response.redirectedUrl == "/SPSSODescriptor/show/${serviceProvider.id}"	
 	}
+
+    def "Validate unsuccessful update with incorrect perms"() {
+        setup:
+        def organization = Organization.build()
+        def entityDescriptor = EntityDescriptor.build(organization:organization)
+        def serviceProvider = SPSSODescriptor.build(entityDescriptor:entityDescriptor).save()
+        
+        controller.params.id = serviceProvider.id
+
+        user.perms.add("descriptor:-1:update")
+        
+        when:
+        def model = controller.update()
+        
+        then:
+        controller.response.status == 403
+    }
 	
 	def "Invalid or non existing SPSSODescriptor fails update"() {
 		setup:		
@@ -212,22 +185,21 @@ class SPSSODescriptorControllerSpec extends IntegrationSpec {
 	
 	def "Invalid service response fails update"() {
 		setup:
-		def organization = Organization.build().save()
-		def entityDescriptor = EntityDescriptor.build(organization:organization).save()
-		def serviceProvider = SPSSODescriptor.build(entityDescriptor:entityDescriptor).save()
+		def organization = Organization.build()
+		def entityDescriptor = EntityDescriptor.build(organization:organization)
+		def serviceProvider = SPSSODescriptor.build(entityDescriptor:entityDescriptor)
 		
 		controller.params.id = serviceProvider.id
-		
-		when:
+        user.perms.add("descriptor:${serviceProvider.id}:update")
 		spssoDescriptorService.metaClass.update = { def p -> 
-			return [false, serviceProvider]
-		} 
+            return [false, serviceProvider]
+        } 
+
+		when:
 		def model = controller.update()
 		
 		then:
-		//organization == controller.modelAndView.model.organization	
-		//entityDescriptor == controller.modelAndView.model.entityDescriptor	
-		//serviceProvider == controller.modelAndView.model.serviceProvider
+		serviceProvider == controller.modelAndView.model.serviceProvider
 		
 		controller.flash.type == "error"
 		controller.flash.message == "fedreg.core.spssoroledescriptor.update.validation.error"	
