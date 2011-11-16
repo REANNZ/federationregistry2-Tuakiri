94bde5135b6ef88508f908ca123dcc919aa83989
Bradley Beddoes <bradleybeddoes@gmail.com>
2011-11-16 15:02:51 +1000

    Updated EntityDescriptorService tests

--- app/plugins/federationcore/test/integration/fedreg/core/EntityDescriptorServiceSpec.groovy
+++ app/plugins/federationcore/test/integration/fedreg/core/EntityDescriptorServiceSpec.groovy
@@ -7,94 +7,49 @@ import fedreg.workflow.*
 import grails.plugins.nimble.core.*
 
 class EntityDescriptorServiceSpec extends IntegrationSpec {
-	def cryptoService
-	def savedMetaClasses
-	def entityDescriptorService
-	def params
+	def cryptoService, entityDescriptorService, params
 	
 	def setup () {
-		savedMetaClasses = [:]
-		
-		entityDescriptorService = new EntityDescriptorService()
+        params = [:]		
 		def user = UserBase.build()
 		SpecHelpers.setupShiroEnv(user)
-		
-		params = [:]
-		
-		EntityDescriptor.findAll()*.delete(flush:true)
-		Organization.findAll()*.delete(flush:true)
 	}
 	
 	def cleanup() {
-		SpecHelpers.resetMetaClasses(savedMetaClasses)
-
 	}
 	
 	def "Creating valid entity descriptor succeeds (without initial contact)"() {
 		setup:
 		def organization = Organization.build().save()
-		def ct = ContactType.build().save()
 		
 		params.organization = [id: organization.id]
 		params.active = true
 		params.entity = [identifier:"http://identityProvider.test.com"]
-		params.contact = [givenName:"Bradley", surname:"Beddoes", email:"bradleybeddoes@intient.com", type:ct.name]
 		
 		when:
 		def(created, entityDescriptor_) = entityDescriptorService.create(params)
 		
 		then:
 		created
-		EntityDescriptor.count() == 1
-		Contact.count() == 1
 		entityDescriptor_ != null
 		entityDescriptor_.organization == organization
-		entityDescriptor_.contacts.size() == 1
-	}
-	
-	def "Creating valid entity descriptor succeeds (with initial contact)"() {
-		setup:
-		def organization = Organization.build().save()
-		def ct = ContactType.build().save()
-		def contact = Contact.build(organization: organization).save()
-		
-		params.organization = [id: organization.id]
-		params.active = true
-		params.entity = [identifier:"http://ed.test.com"]
-		params.contact = [id:contact.id, type:ct.name]
-		
-		when:
-		def(created, entityDescriptor_) = entityDescriptorService.create(params)
-		
-		then:
-		created
-		EntityDescriptor.count() == 1
-		Contact.count() == 1
-		entityDescriptor_ != null
-		entityDescriptor_.organization == organization
-		entityDescriptor_.contacts.size() == 1
 	}
 	
 	def "Creating entity descriptor with invalid data fails"() {
 		setup:
 		def organization = Organization.build().save()
-		def ct = ContactType.build().save()
 		
 		params.organization = [id: organization.id]
 		params.active = true
 		params.entity = [identifier:""]
-		params.contact = [givenName:"Bradley", surname:"Beddoes", email:"bradleybeddoes@intient.com", type:ct.name]
 		
 		when:
 		def(created, entityDescriptor_) = entityDescriptorService.create(params)
 		
 		then:
 		!created
-		EntityDescriptor.count() == 0
-		Contact.count() == 1
 		entityDescriptor_ != null
 		entityDescriptor_.organization == organization
-		entityDescriptor_.contacts.size() == 1
 	}
 	
 	def "Updating entity descriptor with valid data succeeds"() {
@@ -110,7 +65,6 @@ class EntityDescriptorServiceSpec extends IntegrationSpec {
 		
 		then:
 		updated
-		EntityDescriptor.count() == 1
 		entityDescriptor_ != null
 		entityDescriptor_.organization == organization
 		entityDescriptor.entityID == "http://newed.test.com"
@@ -129,7 +83,6 @@ class EntityDescriptorServiceSpec extends IntegrationSpec {
 		
 		then:
 		!updated
-		EntityDescriptor.count() == 1
 		entityDescriptor_.hasErrors()
 		entityDescriptor_ != null
 		entityDescriptor_.organization == organization
@@ -150,7 +103,6 @@ class EntityDescriptorServiceSpec extends IntegrationSpec {
 		
 		then:
 		!updated
-		EntityDescriptor.count() == 2
 		entityDescriptor_.hasErrors()
 		entityDescriptor_.errors.getFieldError('entityID').code == 'unique'
 		entityDescriptor_ != null
