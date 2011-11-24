e207328610c3e752b3acf7e98b4897f7b8904721
Bradley Beddoes <bradleybeddoes@gmail.com>
2011-11-24 16:10:16 +1000

    federationcore upgrade to grails 2.x

--- app/plugins/federationcore/grails-app/conf/BuildConfig.groovy
+++ app/plugins/federationcore/grails-app/conf/BuildConfig.groovy
@@ -6,23 +6,20 @@ grails.plugin.location.nimble="../nimble"
 grails.project.class.dir = "target/classes"
 grails.project.test.class.dir = "target/test-classes"
 grails.project.test.reports.dir	= "target/test-reports"
-grails.project.dependency.resolution = {
-    inherits "global" // inherit Grails' default dependencies
-    log "warn" // log level of Ivy resolver, either 'error', 'warn', 'info', 'debug' or 'verbose'
-    repositories {
-        grailsHome()
 
-        // uncomment the below to enable remote dependency resolution
-        // from public Maven repositories
-        //mavenLocal()
-        //mavenCentral()
-        //mavenRepo "http://snapshots.repository.codehaus.org"
-        //mavenRepo "http://repository.codehaus.org"
-        //mavenRepo "http://download.java.net/maven/2/"
-        //mavenRepo "http://repository.jboss.com/maven2/
+grails.project.dependency.resolution = {
+    inherits "global"
+    log "warn"
+    
+    repositories {
+        grailsPlugins()
+        grailsHome()
+        grailsCentral()
+        mavenCentral()
     }
+
     dependencies {
-        test 'com.mysql:mysql-connector-java:5.1.5'
+        test 'mysql:mysql-connector-java:5.1.18'
     }
 }
 
