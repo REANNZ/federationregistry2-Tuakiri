5a90a24de6588f96e08bf92f31f1f8ead6855a72
Bradley Beddoes <bradleybeddoes@gmail.com>
2012-04-23 09:40:25 +1000

    removed default option

--- app/federationregistry/grails-app/taglib/aaf/fr/FRTagLib.groovy
+++ app/federationregistry/grails-app/taglib/aaf/fr/FRTagLib.groovy
@@ -10,7 +10,7 @@ class FRTagLib {
   static namespace = "fr"
 
   def tooltip = { attrs ->
-    def msg = g.message(code:attrs.code, default:attrs.default)
+    def msg = g.message(code:attrs.code)
     def src = g.resource()
     
     out << r.img(dir:'images', file:'help.png', plugin:'federationregistry', title:msg, width:'16px', height:'16px', rel:'twipsy', 'data-placement':'right')
