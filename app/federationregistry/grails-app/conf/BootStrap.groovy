f4f9de520aff140588a63bcd211f2b7a3017d7d9
Bradley Beddoes <bradleybeddoes@gmail.com>
2012-04-23 16:09:39 +1000

    creation of unrequired roll removed

--- app/federationregistry/grails-app/conf/BootStrap.groovy
+++ app/federationregistry/grails-app/conf/BootStrap.groovy
@@ -5,33 +5,7 @@ import grails.plugins.federatedgrails.Role
 import grails.plugins.federatedgrails.Permission
 
 class BootStrap {
-
-  def roleService
-  def permissionService
-
   def init = { servletContext ->
-    def adminRole = Role.findByName(AdminsService.ADMIN_ROLE)
-    if (!adminRole) {
-      adminRole = new Role()
-      adminRole.description = 'Assigned to users who are considered to be system wide administrator'
-      adminRole.name = AdminsService.ADMIN_ROLE
-      adminRole.protect = true
-      def savedAdminRole = adminRole.save()
-
-      if (adminRole.hasErrors()) {
-          adminRole.errors.each {
-              log.error(it)
-          }
-          throw new RuntimeException("Unable to create valid administrative role")
-      }
-
-      // Grant administrative 'ALL' permission
-      Permission adminPermission = new Permission(target:'*')
-      adminPermission.managed = true
-      adminPermission.type = Permission.adminPerm
-
-      permissionService.createPermission(adminPermission, savedAdminRole)
-    }
   }
 
   def destroy = {
