539c0aa12f38a936f715004c0190f5369693d63a
Bradley Beddoes <bradleybeddoes@gmail.com>
2012-06-22 10:29:32 +1000

    app: Changed configuration to correctly use newer EhCacheRegionFactory impl.

--- app/federationregistry/grails-app/conf/DataSource.groovy
+++ app/federationregistry/grails-app/conf/DataSource.groovy
@@ -1,29 +1,29 @@
 
 hibernate {
-    cache.use_second_level_cache=true
-    cache.use_query_cache=true
-    cache.provider_class='net.sf.ehcache.hibernate.EhCacheProvider'
+    cache.use_second_level_cache = true
+    cache.use_query_cache = true
+    cache.region.factory_class = 'net.sf.ehcache.hibernate.EhCacheRegionFactory'
 }
 
 // environment specific settings
 environments {
 	development {
 		dataSource {
-			dbCreate = "update" // one of 'create', 'create-drop','update'
+			dbCreate = "update"
 		}
 	}
 	test {
 		dataSource {
-            pooled = true
-            driverClassName = "com.mysql.jdbc.Driver"
-            dialect = org.hibernate.dialect.MySQL5InnoDBDialect
-            dbCreate = "create-drop"
-            loggingSql = false
-            
-            url = "jdbc:mysql://localhost/federationregistry_testapp_${System.getenv('fr_testdatabase')}"
-            username = "fr"
-            password =  "password"
-        }
+      pooled = true
+      driverClassName = "com.mysql.jdbc.Driver"
+      dialect = org.hibernate.dialect.MySQL5InnoDBDialect
+      dbCreate = "create-drop"
+      loggingSql = false
+      
+      url = "jdbc:mysql://localhost/federationregistry_testapp_${System.getenv('fr_testdatabase')}"
+      username = "fr"
+      password =  "password"
+    }
 	}
 	production {
 		dataSource {
