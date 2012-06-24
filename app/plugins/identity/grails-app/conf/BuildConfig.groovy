8b67f09ce7c3269a979c18f99b9efebe30210748
Bradley Beddoes <bradleybeddoes@gmail.com>
2012-06-25 09:53:09 +1000

    identity: Changes federated-grails plugin reference from disk to grailsCentral.
    As the federated-grails plugin is now properly published for wider community
    usage FR will now reference it from the upstream source instead of configuring
    it as a local plugin as has been the situation to now.

--- app/plugins/identity/grails-app/conf/BuildConfig.groovy
+++ app/plugins/identity/grails-app/conf/BuildConfig.groovy
@@ -1,7 +1,3 @@
-
-// Local plugins
-grails.plugin.location.federatedgrails="/home/bradleybeddoes/development/federatedgrails"
-
 grails.project.class.dir = "target/classes"
 grails.project.test.class.dir = "target/test-classes"
 grails.project.test.reports.dir = "target/test-reports"
