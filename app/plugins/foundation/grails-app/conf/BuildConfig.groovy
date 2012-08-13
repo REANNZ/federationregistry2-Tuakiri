1f07c5b2624dc6476ab6a0e9be9545562223c7fb
Bradley Beddoes <bradleybeddoes@gmail.com>
2012-08-13 13:18:11 +1000

    app: Upgrade main app and all plugins to use Grails 2 dep resolution.
    See specific details at http://grails.org/doc/2.1.0/guide/conf.html#ivy
    we no longer make use of deprecated 'grails install-plugin X'.
    
    Grails features five dependency resolution configurations (or 'scopes'):
    build: Dependencies for the build system only
    compile: Dependencies for the compile step
    runtime: Dependencies needed at runtime but not for compilation (see above)
    test: Dependencies needed for testing but not at runtime (see above)
    provided: Dependencies needed at development time, but not during WAR deployment
    
    These are used throughout this change.

--- app/plugins/foundation/grails-app/conf/BuildConfig.groovy
+++ app/plugins/foundation/grails-app/conf/BuildConfig.groovy
@@ -8,21 +8,38 @@ grails.project.test.reports.dir = "target/test-reports"
 grails.project.target.level = 1.6
 
 grails.project.dependency.resolution = {
-  inherits("global") {
-      // excludes 'ehcache'
-  }
+  inherits("global") {}
+
   log "warn"
+  checksums true
+  
   repositories {
+    inherits true
+
+    grailsPlugins()
+    grailsHome()
     grailsCentral()
+
+    mavenLocal()
+    mavenCentral()
+
+    mavenRepo "http://snapshots.repository.codehaus.org"
+    mavenRepo "http://repository.codehaus.org"
+    mavenRepo "http://download.java.net/maven/2/"
+    mavenRepo "http://repository.jboss.com/maven2/"
   }
+
   dependencies {
-    compile 'org.grails.plugins:federated-grails:0.2.2'
+    test 'mysql:mysql-connector-java:5.1.18'
   }
 
   plugins {
-    build(":tomcat:$grailsVersion",
-          ":release:1.0.0.RC3") {
-      export = false
-    }
+    build ":tomcat:$grailsVersion"
+    
+    compile ":build-test-data:2.0.3"
+
+    runtime ":hibernate:$grailsVersion"
+
+    test ":spock:0.6"
   }
 }
