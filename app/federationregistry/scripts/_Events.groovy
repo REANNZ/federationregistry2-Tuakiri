087309f633ed4839e4d8fa59c9c3813c721fafd0
Bradley Beddoes <bradleybeddoes@gmail.com>
2012-09-07 13:01:27 +1000

    app: Added ajp connector for development apache mod_proxy_ajp integration.

--- app/federationregistry/scripts/_Events.groovy
+++ app/federationregistry/scripts/_Events.groovy
@@ -1,3 +1,16 @@
+import org.apache.catalina.connector.Connector
+
+eventConfigureTomcat = { tomcat -> 
+  println "Adding AJP connector to Tomcat"
+  def ajpConnector = new Connector("org.apache.coyote.ajp.AjpProtocol") 
+  ajpConnector.port = 8009 
+  ajpConnector.setProperty("redirectPort", "8443") 
+  ajpConnector.setProperty("protocol", "AJP/1.3") 
+  ajpConnector.setProperty("enableLookups", "false") 
+  ajpConnector.setProperty("URIEncoding", "utf-8") 
+  tomcat.service.addConnector ajpConnector 
+}
+
 eventCompileStart = { 
   projectCompiler.srcDirectories << "$basedir/test/common" 
 } 
