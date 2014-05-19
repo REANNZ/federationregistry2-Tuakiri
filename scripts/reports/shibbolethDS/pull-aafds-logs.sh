851f3c89405bdfe761ea1434124bcd91edfb234c
Vladimir Mencl <vladimir.mencl@canterbury.ac.nz>
2014-05-19 15:14:50 +1200

    Scripts/reports: minor exts to pull-aafds-logs.sh
    Minor extensions to scripts/reports/shibbolethDS/pull-aafds-logs.sh
    * allow passing "--dry-run" to be passed to parser script
    * catch failures of wget and parser script and report (and delete incomplete download)

--- scripts/reports/shibbolethDS/pull-aafds-logs.sh
+++ scripts/reports/shibbolethDS/pull-aafds-logs.sh
@@ -10,15 +10,29 @@ DIR_BASE=`dirname $0`
 AAFDS_LOGS="/var/log/aafds"
 PULL_LOG="$AAFDS_LOGS/pull-aafds.log"
 
+PARSER_OPTIONS=""
+if [ "$1" == "--dry-run" ] ; then
+   PARSER_OPTIONS="--dry-run"
+   shift
+fi
+
 cat $DIR_BASE/pull-aafds.conf | while read URL USERNAME PASSWORD LOCALFILE ; do
     URL_TODAY="$URL-$TODAY"
     LOCALFILE_TODAY="$LOCALFILE-$TODAY"
 
     echo "`date` $0: fetching $URL_TODAY into $LOCALFILE_TODAY" >> $PULL_LOG
-    wget -O $LOCALFILE_TODAY --user=$USERNAME --password=$PASSWORD $URL_TODAY >> $PULL_LOG 2>&1
-    echo "`date` $0: invoking $DIR_BASE/parse-aafds-logs.pl $LOCALFILE_TODAY" >> $PULL_LOG
-    $DIR_BASE/parse-aafds-logs.pl $LOCALFILE_TODAY >> $PULL_LOG 2>&1
-
+    wget --quiet -O $LOCALFILE_TODAY --user=$USERNAME --password=$PASSWORD $URL_TODAY >> $PULL_LOG 2>&1
+    if [ $? -ne 0 ] ; then
+         echo "`date` $0: ERROR: fetching $URL_TODAY failed, deleting local file $LOCALFILE_TODAY" >> $PULL_LOG
+         rm $LOCALFILE_TODAY
+         continue
+    fi
+    echo "`date` $0: invoking $DIR_BASE/parse-aafds-logs.pl $PARSER_OPTIONS $LOCALFILE_TODAY" >> $PULL_LOG
+    $DIR_BASE/parse-aafds-logs.pl $PARSER_OPTIONS $LOCALFILE_TODAY >> $PULL_LOG 2>&1
+    if [ $? -ne 0 ] ; then
+         echo "`date` $0: ERROR: parsing $LOCALFILE_TODAY failed" >> $PULL_LOG
+         continue
+    fi
 done
 
 
