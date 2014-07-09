9f7109902101bd88ad2e2df4e69890445fbcf4cb
Bradley Beddoes <bradleybeddoes@gmail.com>
2014-07-09 14:38:20 +1000

    Allow export of IDPSSODescriptor data
    Adds support for FR to continuously export data relating to individial IDPSSODescriptors for use by microservices.

--- app/plugins/export/grails-app/controllers/aaf/fr/export/ExportController.groovy
+++ app/plugins/export/grails-app/controllers/aaf/fr/export/ExportController.groovy
@@ -6,46 +6,68 @@ import aaf.fr.foundation.*
 import groovy.json.JsonSlurper
 
 class ExportController {
-    def organization(long id) {
-      def o = Organization.get(id)
+  def organization(long id) {
+    def o = Organization.get(id)
 
-      if(o) {
-        def builder = new groovy.json.JsonBuilder()
-        builder { organization o.structureAsJson() }
-
-        render text: builder.toPrettyString(), contentType: "text/json"
-      } else {
-        response.status = 404
-        render([error: 'organization is unknown'] as JSON)
-      }
-    }
-
-    def organizations() {
+    if(o) {
       def builder = new groovy.json.JsonBuilder()
-      builder { organizations  Organization.list().collect { o ->
-                              o.structureAsJson()
-                            }
-      }
+      builder { organization o.structureAsJson() }
+
       render text: builder.toPrettyString(), contentType: "text/json"
+    } else {
+      response.status = 404
+      render([error: 'organization is unknown'] as JSON)
     }
+  }
 
-    def serviceprovider(long id) {
-      def sp = SPSSODescriptor.get(id)
+  def organizations() {
+    def builder = new groovy.json.JsonBuilder()
+    builder { organizations  Organization.list().collect { o ->
+                            o.structureAsJson()
+                          }
+    }
+    render text: builder.toPrettyString(), contentType: "text/json"
+  }
 
-      if(sp){
-        def builder = new groovy.json.JsonBuilder()
-        builder { serviceprovider sp.structureAsJson() }
-        render text: builder.toPrettyString(), contentType: "text/json"
-      } else {
-        response.status = 400
-        render([error: 'serviceprovider is unknown'] as JSON)
-      }
+  def serviceprovider(long id) {
+    def sp = SPSSODescriptor.get(id)
+
+    if(sp){
+      def builder = new groovy.json.JsonBuilder()
+      builder { serviceprovider sp.structureAsJson() }
+      render text: builder.toPrettyString(), contentType: "text/json"
+    } else {
+      response.status = 400
+      render([error: 'serviceprovider is unknown'] as JSON)
+    }
   }
 
   def serviceproviders() {
     def builder = new groovy.json.JsonBuilder()
     builder { serviceproviders  SPSSODescriptor.list().collect { sp ->
                             sp.structureAsJson()
+                          }
+    }
+    render text: builder.toPrettyString(), contentType: "text/json"
+  }
+
+  def identityprovider(long id) {
+    def idp = IDPSSODescriptor.get(id)
+
+    if(idp){
+      def builder = new groovy.json.JsonBuilder()
+      builder { identityprovider idp.structureAsJson() }
+      render text: builder.toPrettyString(), contentType: "text/json"
+    } else {
+      response.status = 400
+      render([error: 'identityprovider is unknown'] as JSON)
+    }
+  }
+
+  def identityproviders() {
+    def builder = new groovy.json.JsonBuilder()
+    builder { identityproviders  IDPSSODescriptor.list().collect {idp ->
+                           idp.structureAsJson()
                           }
     }
     render text: builder.toPrettyString(), contentType: "text/json"
