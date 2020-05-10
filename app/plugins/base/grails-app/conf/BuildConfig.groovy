25dd58dcd81badbea49030f1e379531e8c697c71
Vlad Mencl <vladimir.mencl@reannz.co.nz>
2020-05-10 23:50:41 +1200

    Grails2.4: force shiro upgrade from 1.1.3 (pulled by fed-grails) to 1.2.1
    Add spring-aop (and spring-expression) as new dependencies.

--- app/plugins/base/grails-app/conf/BuildConfig.groovy
+++ app/plugins/base/grails-app/conf/BuildConfig.groovy
@@ -27,6 +27,8 @@ grails.project.dependency.resolution = {
 
   dependencies {
     compile "commons-collections:commons-collections:3.2.2"
+    compile "org.springframework:spring-aop:4.0.9.RELEASE" // needed for shiro
+    compile "org.springframework:spring-expression:4.0.9.RELEASE" // needed for tests
 
     test 'mysql:mysql-connector-java:5.1.49'
   }
@@ -35,6 +37,8 @@ grails.project.dependency.resolution = {
     build ":tomcat:7.0.55.2"
 
     compile ":federated-grails:0.6.0"
+    // explicitly require shiro to force upgrade from 1.1.3 pulled by fed-grails
+    compile ":shiro:1.2.1"
     compile ":build-test-data:2.4.0"
     compile ":mail:1.0.7"
     compile ":hibernate4:4.3.8.1"
