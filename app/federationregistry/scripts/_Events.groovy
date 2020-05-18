db068d668dee5f0f98507ebcfa0f944f7452f2cb
Vlad Mencl <vladimir.mencl@reannz.co.nz>
2020-05-18 14:23:07 +1200

    Grails2.5: remove remaining scripts related to aaf-patched-groovy

--- app/federationregistry/scripts/_Events.groovy
+++ app/federationregistry/scripts/_Events.groovy
@@ -17,7 +17,3 @@ eventCompileStart = {
 eventAllTestsStart = {
    classLoader.addURL(new File("$basedir/test/common".toString()).toURL())
 }
-
-eventCreateWarStart = { name, stagingDir ->
-  ant.delete(dir:"${stagingDir}/WEB-INF/lib/", includes: "groovy-all-2.0.8.jar", verbose: true)
-}
